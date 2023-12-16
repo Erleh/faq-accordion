8080c621a16689850acb03c50d25075365fd37be
Eric Lee <eric.lee0051@gmail.com>
2023-12-16 01:42:42 -0800

    Adds base interaction for showing tabs via button, added base styling for mobile design

--- faq-accordion/src/app/card-container/card-tab/card-tab.component.ts
+++ faq-accordion/src/app/card-container/card-tab/card-tab.component.ts
@@ -7,4 +7,12 @@ import { Component, Input } from '@angular/core';
 })
 export class CardTabComponent {
   @Input() data: {title: string, content: string} = {title: '', content: ''};
+  @Input() isLast: boolean = false;
+
+  showContent: boolean = false;
+
+  handleClick() {
+    this.showContent = !this.showContent;
+    console.log(this.showContent);
+  }
 }
